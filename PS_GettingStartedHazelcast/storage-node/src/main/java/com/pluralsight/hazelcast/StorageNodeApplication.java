4f995c04a37970435e0ceca7854abcc148950045
Jerry Hu <hh201020@gmail.com>
2017-04-20 10:56:12 -0400

    working version of cluster

--- PS_GettingStartedHazelcast/storage-node/src/main/java/com/pluralsight/hazelcast/StorageNodeApplication.java
+++ PS_GettingStartedHazelcast/storage-node/src/main/java/com/pluralsight/hazelcast/StorageNodeApplication.java
@@ -1,5 +1,7 @@
 package com.pluralsight.hazelcast;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.SSLConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import org.springframework.boot.SpringApplication;
@@ -17,7 +19,22 @@ public class StorageNodeApplication  {
 
     @Bean(destroyMethod = "shutdown")
     public HazelcastInstance createStorageNode() throws Exception {
-        return Hazelcast.newHazelcastInstance();
+    	Config config = new Config();
+
+    	config.setInstanceName("HazelcastService");
+    	config.getNetworkConfig().setPortAutoIncrement(true);
+    	config.getNetworkConfig().setPort(5701);
+    	config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+    	config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(true);
+    	config.getNetworkConfig().getInterfaces().addInterface("127.0.0.1");
+    	config.getNetworkConfig().getJoin().getTcpIpConfig().addMember("127.0.0.1");
+    	config.getNetworkConfig().getInterfaces().setEnabled(true);
+
+    	SSLConfig sslConfig = new SSLConfig();
+    	sslConfig.setEnabled(false);
+    	config.getNetworkConfig().setSSLConfig(sslConfig);
+
+    	return Hazelcast.newHazelcastInstance(config);
     }
 
 }
